2b60601ba1fb10bbff107d780a027bc82e9688e8
milkmaccya2 <milkmaccya2@gmail.com>
2026-01-19 20:31:13 +0900

    Fix: Add interpreter to ecosystem and start script for debug

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   apps: [{
     name: "thermal-printer-app",
     script: "./dist/server/entry.mjs",
+    interpreter: "node",
     instances: 1,
     exec_mode: "fork",
     env: {
